ea6d2e16dd0d861923868a82f5e46d2f05921598
Yu Jin Choe <forworkchoe@gmail.com>
2026-01-29 08:47:03 +0900

    feat(library): radix-themes 설치

--- apps/examples/src/app/layout.tsx
+++ apps/examples/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type {Metadata} from 'next';
 import {Noto_Sans} from 'next/font/google';
+import '@radix-ui/themes/styles.css';
 import '@monorepo-playground/design-system/style.css';
 import {PropsWithChildren} from 'react';
+import {Theme} from '@radix-ui/themes';
 
 const notoSans = Noto_Sans({
   subsets: ["latin"],
@@ -15,7 +17,9 @@ export default function RootLayout({children}: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={notoSans.className}>
+      <Theme>
         {children}
+      </Theme>
       </body>
     </html>
   );
