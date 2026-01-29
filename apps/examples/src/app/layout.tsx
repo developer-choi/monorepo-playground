9774ad3e7c680516cc569e66729672e13ac58e72
Yu Jin Choe <forworkchoe@gmail.com>
2026-01-29 09:10:36 +0900

    feat(library): Tanstack Query 설치

--- apps/examples/src/app/layout.tsx
+++ apps/examples/src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type {Metadata} from 'next';
 import {Noto_Sans} from 'next/font/google';
-import '@radix-ui/themes/styles.css';
-import '@monorepo-playground/design-system/style.css';
 import {PropsWithChildren} from 'react';
-import {Theme} from '@radix-ui/themes';
+import AppProvider from '@/components/AppProvider';
 
 const notoSans = Noto_Sans({
   subsets: ["latin"],
@@ -17,9 +15,9 @@ export default function RootLayout({children}: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={notoSans.className}>
-      <Theme>
+      <AppProvider>
         {children}
-      </Theme>
+      </AppProvider>
       </body>
     </html>
   );
