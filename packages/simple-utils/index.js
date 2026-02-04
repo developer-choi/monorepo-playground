f2628ada3c38b5a4c9d9d3805d8f080b58cd4ec6
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-05 06:11:01 +0900

    fix(library): 불필요한 초기셋팅용 패키지 삭제

--- packages/simple-utils/index.js
+++
@@ -1,7 +0,0 @@
-export const getCurrentTime = () => {
-  return new Date().toISOString();
-};
-
-export const greet = (name) => {
-  return `Hello, ${name}! Current time is ${getCurrentTime()}`;
-};
