e209b61f565c6da93bbdda47e80a83c306f8a794
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-22 15:18:58 +0900

    feat(example): vercel 환경에서는 파일 읽기 쓰기가 되지않아서 메모리에 데이터를 읽고 쓰기로 변경

--- apps/examples/src/shared/server/database.ts
+++ apps/examples/src/shared/server/database.ts
@@ -1,17 +1,19 @@
-import {readFile, writeFile} from '@/shared/server/file';
 import {ServerBoardDto} from '@/app/api/board/dto';
+import initialData from '@/shared/server/database/board.json';
 
 interface BoardTable {
   list: ServerBoardDto[];
 }
 
+let boardStore: BoardTable = initialData;
+
 const database = {
   board: {
-    get: function getBoard() {
-      return readFile<BoardTable>('database/board.json');
+    get: function getBoard(): Promise<BoardTable> {
+      return Promise.resolve(boardStore);
     },
-    set: async function setBoard(board: BoardTable) {
-      return writeFile('database/board.json', JSON.stringify(board));
+    set: async function setBoard(board: BoardTable): Promise<void> {
+      boardStore = board;
     },
   },
 };
