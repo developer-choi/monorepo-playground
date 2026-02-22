296a1942cd244f45fc356000335928b56e70c5ff
Yu Jin Choe <forworkchoe@gmail.com>
2026-02-22 20:50:24 +0900

    fix(examples): ky 인스턴스 만들 때 불필요한 설정 삭제

--- apps/examples/src/shared/api/client.ts
+++ apps/examples/src/shared/api/client.ts
@@ -1,9 +1,5 @@
 import ky from 'ky';
 
 export const api = ky.create({
-  prefixUrl: process.env.NEXT_PUBLIC_API_URL,
-  retry: {
-    limit: 1,
-    methods: ['get', 'post', 'put', 'patch', 'delete', 'head'],
-  },
+  prefixUrl: process.env.NEXT_PUBLIC_API_URL
 });
