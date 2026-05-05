71b8518fb872d05b456800c9cf280203aaac160b
Yu Jin Choe <forworkchoe@gmail.com>
2026-05-05 23:38:49 +0900

    fix(examples): Sentry Wizard 설치

---
+++ apps/examples/sentry.edge.config.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://62de46a2edebd4a30601b4179549c068@o4508301096714240.ingest.us.sentry.io/4511337766780928',
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
